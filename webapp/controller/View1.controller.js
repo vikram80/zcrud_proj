5625f760bd6a395f490753b9ee74c2e0907c210d
Vikram80 <vikramjalali@gmail.com>
2019-07-19 19:58:55 +0000

    Commit1

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("zcrud_proj.zcrud_proj.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zcrud_proj/zcrud_proj/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
